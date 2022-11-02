b83e8b50689a1047edb9c922863ab93b5ac48495
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-11-02 11:42:39 +0200

    [feat] hash client password

--- sneakersStore/src/main/java/com/codecool/sneakersStore/controller/ClientController.java
+++ sneakersStore/src/main/java/com/codecool/sneakersStore/controller/ClientController.java
@@ -2,6 +2,7 @@ package com.codecool.sneakersStore.controller;
 
 import com.codecool.sneakersStore.model.Client;
 import com.codecool.sneakersStore.service.ClientService;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,7 +24,14 @@ public class ClientController {
 
     @PostMapping(value = "add-client")
     public void addClient(@RequestBody Client client){
-        Client client1 = new Client(client.getId(), client.getFirstName(), client.getLastName(), client.getEmail(), client.getPassword());
+        Argon2PasswordEncoder encoder = new Argon2PasswordEncoder(32,64,1,15*1024,2);
+        String textPassword = client.getPassword();
+        String encodedPassword = encoder.encode(textPassword);
+
+        Boolean validPassword = encoder.matches(textPassword, encodedPassword);
+        System.out.println(validPassword);
+
+        Client client1 = new Client(client.getId(), client.getFirstName(), client.getLastName(), client.getEmail(), encodedPassword);
         clientService.addClient(client1);
     }
 
