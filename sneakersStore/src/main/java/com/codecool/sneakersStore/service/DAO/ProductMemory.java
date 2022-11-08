8d95900c548e23ab8090cdc068d98c3e455af6c8
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-11-08 09:53:46 +0200

    [feat] solved conflicts after merge

--- sneakersStore/src/main/java/com/codecool/sneakersStore/service/DAO/ProductMemory.java
+++ sneakersStore/src/main/java/com/codecool/sneakersStore/service/DAO/ProductMemory.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 public class ProductMemory {
-    private List<Product> products;
+    private final List<Product> products;
 
     public ProductMemory() {
         this.products = new ArrayList<>();
