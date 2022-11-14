bd0769fd7cab719eae7aedb42b58f13af9012e97
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-11-14 15:39:05 +0200

    push after resolved conflicts

--- client/src/components/layout/Layout.js
+++ client/src/components/layout/Layout.js
@@ -1,12 +1,10 @@
 import Footer from "./Footer";
-import Header from "./Header";
 import Navbar from "./Navbar";
 
 function Layout(props) {
     return (
         <div>
             <Navbar/>
-
             {props.children}
             <Footer />
         </div>
