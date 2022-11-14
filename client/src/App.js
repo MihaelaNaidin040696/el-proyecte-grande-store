7625576c3306fc972e4a26e2e743d6fa7c4bc1ba
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-11-14 14:04:15 +0200

    [feat] created Home page with Carousel for products pictures

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,13 @@ import Logout from "./pages/Logout";
 import Cart from './pages/Cart';
 import Clothes from './pages/Clothes';
 import Sneakers from './pages/Sneakers';
+import Home from "./pages/Home";
 
 function App() {
     return (
         <Layout>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/logout" element={<Logout />} />
---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import ProductsCarousel from "../components/layout/Carousel";
+
+export default function Home() {
+    return (
+        <div>
+            <ProductsCarousel />
+        </div>
+    );
+}
+
+
+
