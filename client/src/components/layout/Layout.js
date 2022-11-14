1e3a6061ce28bb4adb26b369a7d2bfa19662056d
GeorgeAnghel114 <georgeanghel114@gmail.com>
2022-11-14 13:10:57 +0200

    [feat] navbar responsive + changed colors

--- client/src/components/layout/Layout.js
+++ client/src/components/layout/Layout.js
@@ -5,7 +5,6 @@ import Navbar from "./Navbar";
 function Layout(props) {
     return (
         <div>
-            <Header/>
             <Navbar/>
 
             {props.children}
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,21 +1,41 @@
 import classes from './Navbar.module.css';
+import {useState} from "react";
 
 export default function Navbar() {
+    const[mobileNavbar, setMobileNavbar] = useState(false);
+
     return (
         <div className={classes.container}>
             <nav>
                 <label className={classes.logo}>RIGHT Sneakers Store</label>
                 <ul className={classes.left}>
-                    <li><a className={classes.active} href='/'>Home</a></li>
-                    <li><a href='/sneakers'>Sneakers</a></li>
-                    <li><a href='/clothes'>Clothes</a></li>
+                    <li><a className={classes.aNav} href='/'>Home</a></li>
+                    <li><a className={classes.aNav} href='/sneakers'>Sneakers</a></li>
+                    <li><a className={classes.aNav} href='/clothes'>Clothes</a></li>
                 </ul>
+                <label className={classes.hamburger}>
+                     <i className="fas fa-bars" onClick={()=>setMobileNavbar(!mobileNavbar)}></i>
+                </label>
                 <ul className={classes.right}>
-                    <li><a href='/cart'>Cart</a></li>
-                    <li><a href='/login'>Login</a></li>
-                    <li><a href='/register'>Register</a></li>
-                    <li><a href='/logout'>Logout</a></li>
+                    <li><a className={classes.aNav} href='/cart'>Cart</a></li>
+                    <li><a className={classes.aNav} href='/login'>Login</a></li>
+                    <li><a className={classes.aNav} href='/register'>Register</a></li>
+                    <li><a className={classes.aNav} href='/logout'>Logout</a></li>
                 </ul>
+                {mobileNavbar && <div className={classes.mobileContainer}>
+                    <label className={classes.hamburger}>
+                        <i className="fas fa-close" onClick={()=>setMobileNavbar(!mobileNavbar)}></i>
+                    </label>
+                    <ul className={classes.mobileNav}>
+                        <li><a className={classes.aNav} href='/'>Home</a></li>
+                        <li><a className={classes.aNav} href='/sneakers'>Sneakers</a></li>
+                        <li><a className={classes.aNav} href='/clothes'>Clothes</a></li>
+                        <li><a className={classes.aNav} href='/cart'>Cart</a></li>
+                        <li><a className={classes.aNav} href='/login'>Login</a></li>
+                        <li><a className={classes.aNav} href='/register'>Register</a></li>
+                        <li><a className={classes.aNav}href='/logout'>Logout</a></li>
+                    </ul>
+                </div>}
             </nav>
         </div>
     );
