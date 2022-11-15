0bf6756617062cc6405b2fd3f43d45a6c6abe64d
GeorgeAnghel114 <georgeanghel114@gmail.com>
2022-11-15 17:04:20 +0200

    getProductById on pressing the cart of each product

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Cart from './pages/Cart';
 import Clothes from './pages/Clothes';
 import Sneakers from './pages/Sneakers';
 import Home from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
     return (
@@ -17,6 +18,8 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/logout" element={<Logout />} />
                 <Route path="/sneakers" element={<Sneakers />} />
+                <Route path="/product/:id" element={<ProductPage />} />
+
                 <Route path="/clothes" element={<Clothes />} />
                 <Route path="/cart" element={<Cart />} />
             </Routes>
--- client/src/components/layout/ProductCard.js
+++ client/src/components/layout/ProductCard.js
@@ -1,22 +1,34 @@
 import {FaShoppingCart} from 'react-icons/fa';
 import classes from './Products.module.css';
+import { useNavigate } from "react-router-dom";
 
 export default function ProductCard(props) {
+    const navigate = useNavigate();
+
     return (
         <>
             <div key={props.id} className={classes.productCard}>
-
-                <FaShoppingCart className={classes.productCard__cart}/>
+              
+                     <FaShoppingCart onClick={()=>{navigate("/product/"+props.id)}}
+                        className={classes.productCard__cart}
+                        values={props.id}/>
 
                 <div className={classes.productCard__content}>
-                    <img src={props.image} alt={props.name} className={classes.productImage}/>
+                    <img
+                        src={props.image}
+                        alt={props.name}
+                        className={classes.productImage}
+                    />
                     <p className={classes.productName}>{props.name}</p>
                     <div className={classes.displayStack__1}>
-                        <div className={classes.productPrice}>${props.sellingPrice}</div>
-                        <div className={classes.productSales}>{props.size} size</div>
+                        <div className={classes.productPrice}>
+                            ${props.sellingPrice}
+                        </div>
+                        <div className={classes.productSales}>
+                            {props.size} size
+                        </div>
                     </div>
                 </div>
-
             </div>
         </>
     );
