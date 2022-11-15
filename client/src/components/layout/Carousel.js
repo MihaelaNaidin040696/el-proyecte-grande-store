435a659b67ec1efac5611f2cf05810be8872cb50
GeorgeAnghel114 <georgeanghel114@gmail.com>
2022-11-15 10:04:48 +0200

    [fix] fixed bug on navbar

--- client/src/components/layout/Carousel.js
+++ client/src/components/layout/Carousel.js
@@ -4,9 +4,9 @@ import classes from './Carousel.module.css';
 export default function ProductsCarousel() {
     const breakPoints = [
         {width: 1, itemsToShow: 1},
-        {width: 550, itemsToShow: 2},
-        {width: 768, itemsToShow: 3},
-        {width: 1200, itemsToShow: 4},
+        // {width: 550, itemsToShow: 2},
+        // {width: 768, itemsToShow: 3},
+        // {width: 1200, itemsToShow: 4},
     ]
 
     return (
