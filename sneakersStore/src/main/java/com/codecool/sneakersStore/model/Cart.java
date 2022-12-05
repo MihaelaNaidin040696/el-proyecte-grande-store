31f99e243ee8c79c4c8f16710f9df873d3cdbab9
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-12-05 11:00:10 +0200

    [feat] create cart and cart item entities

--- sneakersStore/src/main/java/com/codecool/sneakersStore/model/Cart.java
+++ sneakersStore/src/main/java/com/codecool/sneakersStore/model/Cart.java
@@ -1,15 +1,33 @@
 package com.codecool.sneakersStore.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
 
+@Entity
+@Table(name = "cart")
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class Cart {
-    private List<CartItem> cartItems;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "cart")
+    private Set<CartItem> cartItems;
 
 }
--- sneakersStore/src/main/java/com/codecool/sneakersStore/model/CartItem.java
+++ sneakersStore/src/main/java/com/codecool/sneakersStore/model/CartItem.java
@@ -1,17 +1,48 @@
 package com.codecool.sneakersStore.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.HashMap;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "cart_items")
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @OneToOne
     private Product product;
-    private int quantity;
-    private float total;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", referencedColumnName = "id")
+    private Cart cart;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "total")
+    private Float total;
 
 }
