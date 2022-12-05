98df6c9a7a49a8abb10aef8999ce4ff0b7bd90fd
GeorgeAnghel114 <georgeanghel114@gmail.com>
2022-12-05 11:14:45 +0200

    [feat] createdb and tables: product,brand,category,supplier

--- sneakersStore/src/main/java/com/codecool/sneakersStore/SneakersStoreApplication.java
+++ sneakersStore/src/main/java/com/codecool/sneakersStore/SneakersStoreApplication.java
@@ -1,19 +1,11 @@
 package com.codecool.sneakersStore;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-public class SneakersStoreApplication implements CommandLineRunner {
-
+@SpringBootApplication
+public class SneakersStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SneakersStoreApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
 	}
 }
--- sneakersStore/src/main/java/com/codecool/sneakersStore/model/Brand.java
+++ sneakersStore/src/main/java/com/codecool/sneakersStore/model/Brand.java
@@ -1,4 +1,32 @@
 package com.codecool.sneakersStore.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Getter
+@Setter
+@Table(name = "brands")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "brand")
+    private Set<Product> products;
+
+
 }
