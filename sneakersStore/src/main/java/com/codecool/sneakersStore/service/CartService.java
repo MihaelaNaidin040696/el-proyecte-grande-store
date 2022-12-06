72b8f8dd15c1d55a9ac35372d2404beb6a827b17
GeorgeAnghel114 <georgeanghel114@gmail.com>
2022-12-06 09:40:45 +0200

    [feat] getCary by id

--- sneakersStore/src/main/java/com/codecool/sneakersStore/service/CartService.java
+++ sneakersStore/src/main/java/com/codecool/sneakersStore/service/CartService.java
@@ -1,20 +1,34 @@
 package com.codecool.sneakersStore.service;
 
 import com.codecool.sneakersStore.model.Cart;
+import com.codecool.sneakersStore.model.CartItem;
+import com.codecool.sneakersStore.repository.CartItemRepository;
 import com.codecool.sneakersStore.repository.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 @Service
 public class CartService {
     private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
 
     @Autowired
-    public CartService(CartRepository cartRepository) {
+    public CartService(CartRepository cartRepository, CartItemRepository cartItemRepository) {
         this.cartRepository = cartRepository;
+        this.cartItemRepository = cartItemRepository;
     }
 
     public void addCart(Cart cart) {
         cartRepository.save(cart);
+    }
+
+    public Cart getCartById(Long id){
+        Optional<Cart> optionalCart = cartRepository.findById(id);
+        return optionalCart.orElse(null);
+    }
+
+    public void addCartItemToCart(CartItem cartItem){
+
     }
 }
