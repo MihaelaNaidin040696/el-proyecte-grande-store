4974fa0220c1463f9e99cde2b2db115c5bf01c0a
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-12-07 14:06:22 +0200

    [fix] resize product cards to fit content

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,8 @@ function Home() {
     const [isLoading, setIsLoading] = useState(true);
     const [loadedProducts, setLoadedProducts] = useState([]);
 
+    // useAtom - READ ABOUT IT !!!
+
     useEffect(() => {
 
         setIsLoading(true);
