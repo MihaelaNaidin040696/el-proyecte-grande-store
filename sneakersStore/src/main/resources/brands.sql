0d00de991beb40a1410751e7b475ce650e2248b7
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2022-12-07 11:53:57 +0200

    [feat] populate database tables, generate scripts

---
+++ sneakersStore/src/main/resources/brands.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.brands (id, name) VALUES (1, 'JORDAN');
+INSERT INTO public.brands (id, name) VALUES (2, 'NIKE');
+INSERT INTO public.brands (id, name) VALUES (3, 'PUMA');
+INSERT INTO public.brands (id, name) VALUES (4, 'ADIDAS ORIGINALS');
+INSERT INTO public.brands (id, name) VALUES (5, 'CONVERSE');
+INSERT INTO public.brands (id, name) VALUES (6, 'NEW BALANCE');
