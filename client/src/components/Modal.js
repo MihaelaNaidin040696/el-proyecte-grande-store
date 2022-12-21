5385f2f070606a610ca671fe432589139c7669f9
GeorgeAnghel114 <georgeanghel114@gmail.com>
2022-12-21 11:11:29 +0200

    [feat] increase quantity of cart item in cart front end

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,21 +1,58 @@
+import { South } from "@mui/icons-material";
 import React,{ useEffect, useState } from "react";
 import classes from "./Modal.module.css";
-
 
 
 export default function Modal({ setModal }) {
     const [cart,setCart] = useState([])
     const [cartItems,setCartItems] = useState()
+    const [itemQty, setItemQty] = useState({id:'', quantity:'' })
+    const [qty,setQty] = useState();
+    console.log(cart)
+    console.log(cartItems);
+    
+    function addItemQty(e){
+        let newList = [];
+        let obj = {id: '', quantity: ''}
+        let total = 0;
+        for (let index = 0; index < cartItems.length; index++) {
+            const element = cartItems[index];
+            const p = element.product.id
+            console.log(p, e.currentTarget.id)
+        if(p == e.currentTarget.id){
+            const sellingPer = element.product.sellingPrice;
+            element.quantity += 1;
+            element.totalPrice = element.quantity * sellingPer;
+            
+        }
+        
+        newList.push(element);
+        total += element.totalPrice;
+        cart.totalPrices = total;
+        obj.id = p;
+        obj.quantity = element.quantity;
+        setItemQty({id:p,quantity:element.quantity+1})
+
+        console.log(itemQty);
+        }
+        
+        setCart(cart);
+        setCartItems(newList);
+
+
+    }
 
     function closeModal() {
         setModal(false);
-        // document.body.style.overflow = "visible";
     }
 
-    console.log(cartItems?.length);
+    useEffect(() =>{
+        const fetchCart = async ()=>{
+            const request = await fetch()
+        }
+        console.log('Am 3 leeeeeei')
+    }, [cartItems])
 
-    
-    // console.log(cartItems)
     useEffect(()=>{
         const fetchCart = async ()=>{
             const request = await fetch("http://localhost:8080/cart/get-cart")
@@ -58,7 +95,12 @@ export default function Modal({ setModal }) {
                                      <div className={classes.content}>
                                         <h3>{item.product.productName}</h3> 
                                         <h4>{item.product.sellingPrice * item.quantity} $</h4>
-                                        <p className={classes.unit}>Quantity: <button>-</button><input value={item.quantity }/><button>+</button></p>
+                                        <p className={classes.unit}>Quantity:
+                                         <button>-</button>
+                                         <input 
+                                         onChange={(e)=> setQty(item.quantity)}
+                                         value={item.quantity}/>
+                                         <button onClick={addItemQty} id={item.product.id}>+</button></p>
                                         <p className={classes.btnArea}>
                                             <i className="fa fa-trash"></i>
                                             <span className={classes.btn2} id={item.product.id} onClick={fetchDeleteItemFromCart}>Remove</span>
