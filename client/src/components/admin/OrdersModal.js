34d0e53326e542386ec8f50faeaa61998ad05b0f
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2023-01-17 13:45:21 +0200

    [feat] modal with order details done

--- client/src/components/admin/OrdersModal.js
+++ client/src/components/admin/OrdersModal.js
@@ -1,5 +1,6 @@
 import classes from './OrdersModal.module.css';
 import React from "react";
+import moment from "moment/moment";
 
 export default function OrdersModal({setModal, order}) {
 
@@ -9,24 +10,68 @@ export default function OrdersModal({setModal, order}) {
 
     return (
         <div className={classes.container}>
-            <div className={classes.modalContainer}>
-                <>
-                    <div className={classes.close_container}>
-                        <h1 className={classes.wrapper}>Order Details</h1>
+            <div>
+                <div>
                         <span className={classes.close_btn}>
                             <i className="fas fa-close" onClick={closeModal}></i>
                         </span>
-                    </div>
-                    <div className={classes.project}>
-                        <div className={classes.shop}>
-                            <div className={classes.content}>
-                                <h3>{order.first_name}</h3>
-                                <h3>{order.last_name}</h3>
-                                <h4>{order.totalPrice} $</h4>
-                            </div>
-                        </div>
-                    </div>
-                </>
+                </div>
+                <div className={classes.box}>
+                    <table>
+                        <h3 style={{marginLeft: '50px', fontStyle: 'oblique'}}>CUSTOMER</h3>
+
+                            <tr>
+                                <th>Name:</th>
+                                <td>{order.first_name} {order.last_name}</td>
+                            </tr>
+                            <tr>
+                                <th>Email:</th>
+                                <td>{order.email}</td>
+                            </tr>
+                            <tr>
+                                <th>Phone number:</th>
+                                <td>{order.phone_number}</td>
+                            </tr>
+
+                        <h3 style={{marginTop: '30px', marginLeft: '50px', fontStyle: 'oblique'}}>DELIVERY</h3>
+
+                            <tr>
+                                <th>Country:</th>
+                                <td>{order.country}</td>
+                            </tr>
+                            <tr>
+                                <th>City:</th>
+                                <td>{order.city}</td>
+                            </tr>
+                            <tr>
+                                <th>Address:</th>
+                                <td>{order.address}</td>
+                            </tr>
+                            <tr>
+                                <th>Postal Code:</th>
+                                <td>{order.postal_code}</td>
+                            </tr>
+                            <tr>
+                                <th>Notes:</th>
+                                <td>{order.notes}</td>
+                            </tr>
+
+                        <h3 style={{marginTop: '30px', marginLeft: '60px', fontStyle: 'oblique'}}>ORDER</h3>
+
+                            <tr>
+                                <th>Order Date:</th>
+                                <td>{moment(order.orderDate).format('DD-MM-YYYY')}</td>
+                            </tr>
+                            <tr>
+                                <th>Total Amount:</th>
+                                <td>{order.totalPrice}</td>
+                            </tr>
+                            <tr>
+                                <th>Tracking ID:</th>
+                                <td>{order.trackingId}</td>
+                            </tr>
+                    </table>
+                </div>
             </div>
         </div>
     );
