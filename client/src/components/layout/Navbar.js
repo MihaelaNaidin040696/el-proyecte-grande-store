27b7f60ea0040a5559ec125d2dcee9ef5f8f5011
GeorgeAnghel114 <georgeanghel114@gmail.com>
2023-01-18 18:11:17 +0200

    [feat] logged in username on frontend

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,21 +1,162 @@
 import classes from "./Navbar.module.css";
-import { useState } from "react";
+import { useState, useEffect,useReducer } from "react";
 import Modal from "../user/Modal";
 
-export default function Navbar() {
+export default function Navbar({name}) {
     const [mobileNavbar, setMobileNavbar] = useState(false);
     const [modal, setModal] = useState(false);
+    const [username, setUsername] = useState("")
+
     function openModal() {
         setModal(true);
         document.body.style.overflow = "visible";
         setMobileNavbar(false);
     }
 
+
+    function test(){
+        let res = localStorage.getItem("username")
+        setUsername(localStorage.getItem(res))
+
+    }
+    useEffect(() => {
+        console.log(localStorage.getItem("username"))
+        test();
+
+    },[username])
+
+    async function logout() {
+        localStorage.clear();
+      };
+    
     return (
         <>
-            <div className={classes.container}>
+        {name
+            ? (<>
+             <div className={classes.container}>
                 <nav>
                     <label className={classes.logo}>RIGHT Sneakers Store</label>
+                    
+                    <ul className={classes.left}>
+                        <li>
+                            <a className={classes.aNav} href="/">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a className={classes.aNav} href="/sneakers">
+                                Sneakers
+                            </a>
+                        </li>
+                        <li>
+                            <a className={classes.aNav} href="/clothes">
+                                Clothes
+                            </a>
+                        </li>
+                    </ul>
+                    <label className={classes.hamburger}>
+                        <i
+                            className="fas fa-bars"
+                            onClick={() => setMobileNavbar(!mobileNavbar)}
+                        ></i>
+                    </label>
+                    <ul className={classes.right}>
+                        <li
+                            onClick={() => openModal()}
+                            className={classes.cartNav}
+                        >
+                            <div>Cart</div>
+                        </li>
+
+                        <li>
+                            <a className={classes.aNav}>
+                                {name}
+                            </a>
+                        </li>
+                        {/* <li>
+                            <a className={classes.aNav} href="/register">
+                                Register
+                            </a>
+                        </li> */}
+                        <li>
+                            <a className={classes.aNav} onClick={logout} href="/">
+                                Logout
+                            </a>
+                        </li>
+                    </ul>
+
+
+                    {mobileNavbar && (
+                        <div className={classes.mobileContainer}>
+                            <label className={classes.hamburger}>
+                                <i
+                                    className="fas fa-close"
+                                    onClick={() =>
+                                        setMobileNavbar(!mobileNavbar)
+                                    }
+                                ></i>
+                            </label>
+                            <ul className={classes.mobileNav}>
+                                <li>
+                                    <a className={classes.aNav} href="/">
+                                        Home
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        className={classes.aNav}
+                                        href="/sneakers"
+                                    >
+                                        Sneakers
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className={classes.aNav} href="/clothes">
+                                        Clothes
+                                    </a>
+                                </li>
+                                <li
+                                    onClick={() => openModal()}
+                                    className={classes.cartNav}
+                                >
+                                    <div>Cart</div>
+                                </li>
+                                <li>
+                                    <a className={classes.aNav} href="/login">
+                                        Login
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        className={classes.aNav}
+                                        href="/register"
+                                    >
+                                        Register
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className={classes.aNav} onClick={logout}>
+                                        Logout
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    )}
+                </nav>
+            </div>
+            {modal && <Modal setModal={setModal} />}
+ 
+            </>) 
+            
+
+            :
+            
+            
+            (<>
+                <div className={classes.container}>
+                <nav>
+                    <label className={classes.logo}>RIGHT Sneakers Store</label>
+                    
                     <ul className={classes.left}>
                         <li>
                             <a className={classes.aNav} href="/">
@@ -57,12 +198,14 @@ export default function Navbar() {
                                 Register
                             </a>
                         </li>
-                        <li>
+                        {/* <li>
                             <a className={classes.aNav} href="/logout">
                                 Logout
                             </a>
-                        </li>
+                        </li> */}
                     </ul>
+
+
                     {mobileNavbar && (
                         <div className={classes.mobileContainer}>
                             <label className={classes.hamburger}>
@@ -122,6 +265,9 @@ export default function Navbar() {
                 </nav>
             </div>
             {modal && <Modal setModal={setModal} />}
-        </>
+ 
+            </>)
+        }
+                  </>
     );
 }
